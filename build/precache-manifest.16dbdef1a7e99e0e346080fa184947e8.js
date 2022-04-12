5debc216829af6037e160fdd61e2be470b0aeb88
Freespirit <66442151+Jose-Tom@users.noreply.github.com>
2022-04-12 13:36:19 +0530

    deployment
    final build

---
+++ build/precache-manifest.16dbdef1a7e99e0e346080fa184947e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132d2f0d0cdb9d151f9c5ce7f7173b87",
+    "url": "/olx-clone/index.html"
+  },
+  {
+    "revision": "804ae732cec2479cf334",
+    "url": "/olx-clone/static/css/main.c3bc984f.chunk.css"
+  },
+  {
+    "revision": "7889df2ad0bc11b5b580",
+    "url": "/olx-clone/static/js/2.d58f8055.chunk.js"
+  },
+  {
+    "revision": "9dfbab745ddad106d73206c378a68235",
+    "url": "/olx-clone/static/js/2.d58f8055.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "804ae732cec2479cf334",
+    "url": "/olx-clone/static/js/main.00ff69e5.chunk.js"
+  },
+  {
+    "revision": "29e2fbba14875e69eb99",
+    "url": "/olx-clone/static/js/runtime-main.f2a2715e.js"
+  },
+  {
+    "revision": "1202463180d7c17ec35b9095f643efe8",
+    "url": "/olx-clone/static/media/olx-logo.12024631.png"
+  }
+]);
