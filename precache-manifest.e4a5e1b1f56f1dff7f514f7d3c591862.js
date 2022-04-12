bd5f2be81ba91db300a22d51d65b1ce850a077d7
Freespirit <66442151+Jose-Tom@users.noreply.github.com>
2022-04-12 13:57:55 +0530

    Updates

---
+++ precache-manifest.e4a5e1b1f56f1dff7f514f7d3c591862.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a054ec5c4c666e887fe13f210733022",
+    "url": "/olx-clone/index.html"
+  },
+  {
+    "revision": "cb9c0bd60b2906d3b262",
+    "url": "/olx-clone/static/css/main.c3bc984f.chunk.css"
+  },
+  {
+    "revision": "7889df2ad0bc11b5b580",
+    "url": "/olx-clone/static/js/2.d58f8055.chunk.js"
+  },
+  {
+    "revision": "9dfbab745ddad106d73206c378a68235",
+    "url": "/olx-clone/static/js/2.d58f8055.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb9c0bd60b2906d3b262",
+    "url": "/olx-clone/static/js/main.bc307ad2.chunk.js"
+  },
+  {
+    "revision": "29e2fbba14875e69eb99",
+    "url": "/olx-clone/static/js/runtime-main.f2a2715e.js"
+  },
+  {
+    "revision": "1202463180d7c17ec35b9095f643efe8",
+    "url": "/olx-clone/static/media/olx-logo.12024631.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/olx-clone/precache-manifest.7526bbd7e842551c483d39e15ed1f4e6.js"
+  "/olx-clone/precache-manifest.e4a5e1b1f56f1dff7f514f7d3c591862.js"
 );
 
 self.addEventListener('message', (event) => {
